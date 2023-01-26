a9f2070da3381c407e3e70d0a7ae0da9f4a554ee
That0neg1nger <122539567+That0neg1nger@users.noreply.github.com>
2023-01-26 08:46:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    coun += 1
+    basic.showNumber(coun)
+})
+input.onButtonPressed(Button.AB, function () {
+    coun = 0
+    basic.showNumber(coun)
+})
+input.onButtonPressed(Button.B, function () {
+    coun += -1
+    basic.showNumber(coun)
+})
+let coun = 0
+coun = 0
+basic.forever(function () {
+	
+})
